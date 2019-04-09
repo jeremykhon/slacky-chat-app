50d6c0229dd993c072f7a753113902c7520222f8
Jeremy Hon <jehon11@gmail.com>
2019-04-09 14:53:33 +0800

    added models, seed, and messages controller

---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::MessagesController < ApplicationController
+  before_action :set_channel
+
+  def index
+    messages = @channel.messages.order('created_at ASC')
+    render json: messages
+  end
+
+  def create
+    message = @channel.messages.build(content: params[:content])
+    message.user = user
+    message.save
+    render json: message
+  end
+
+  private
+
+  def set_channel
+    @channel = Channel.find_by(name: params[:channel_id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+names = %w(general hongkong hello)
+nicknames = %w(Jeremy Maya monsieurpaillard krokrob Eschults)
+
+channels = names.map do |name|
+  Channel.find_or_create_by(name: name)
+end
+
+users = nicknames.map do |nickname|
+  User.create(email: "#{nickname.downcase}@lewagon.com", nickname: nickname, password: "testtest")
+end
+
+20.times do
+  Message.create! user: users.sample, channel: channels.sample, content: Faker::HowIMetYourMother.quote
+end
+
+puts 'Channels:'
+channels.each do |channel|
+  puts "- #{channel.id}: #{channel.name}"
+end
