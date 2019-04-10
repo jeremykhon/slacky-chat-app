f3104c00ae5f1585fa0e1cb2f2ccb2bfcbb340d8
Jeremy Hon <jehon11@gmail.com>
2019-04-10 13:57:33 +0800

    added date divider

--- app/javascript/chat/containers/message_list.jsx
+++ app/javascript/chat/containers/message_list.jsx
@@ -41,13 +41,58 @@ class MessageList extends Component {
     );
   }
 
+  calcDate = (message) => {
+    if (message === undefined) {
+      return null;
+    }
+    const date = new Date(message.created_at).toLocaleDateString([], {year: '2-digit', month:'2-digit', day:'2-digit'})
+    return date
+  }
+
+  calcLongDate = (message) => {
+    if (message === undefined) {
+      return null;
+    }
+    const date = new Date(message.created_at).toLocaleDateString([], {year: '2-digit', month:'short', day:'2-digit', weekday:'long'})
+    return date
+  }
+
+  groupByDateName = (messages) => {
+    const groupedByDate = []
+    let temp = []
+    messages.forEach((message, index) => {
+      if (index === 0) {
+        temp.push(message);
+      } else if (this.calcDate(message) !== this.calcDate(messages[index-1])) {
+        groupedByDate.push(temp);
+        temp = [];
+        temp.push(message);
+      } else {
+        temp.push(message);
+      }
+    })
+    groupedByDate.push(temp)
+    
+    return groupedByDate.map((dayChunk, index) => {
+      console.log(dayChunk)
+      return (
+        <div key={index}>
+          <div className="date-divider text-center">
+            {this.calcLongDate(dayChunk[0])}
+          </div>
+          {dayChunk.map(message => <Message message={message} key={message.id} />)}
+        </div>
+      )
+    })
+  }
+
   render() {
     const { messages } = this.props;
     return (
       <div className="col-10 right-container">
         <div className="channel-title">{`#${this.props.selectedChannel}`}</div>
         <div className="message-list" ref={(list) => { this.list = list; }}>
-          {messages.map(message => <Message message={message} key={message.id} />)}
+          {this.groupByDateName(messages)}
         </div>
         {<MessageForm selectedChannel={this.props.selectedChannel}/>}
       </div>
