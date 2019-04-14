4e42a0bb6129bb3b258a16340db506aa04b84450
Jeremy Hon <jehon11@gmail.com>
2019-04-14 18:04:36 +0800

    removed devise controllers, added nickname field to devise registrations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :channel
-  validates :content, presence: true
+  validates :content, :nickname, presence: true
   after_create :broadcast_message
 
   def as_json(options = {})
